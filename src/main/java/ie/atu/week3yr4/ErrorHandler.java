f70dd4fb47f922d72d470997c962ef8c4fd712a3
Just0jo <G00407342@atu.ie>
2024-09-30 13:15:47 +0100

    error handler

--- src/main/java/ie/atu/week3yr4/ErrorHandler.java
+++ src/main/java/ie/atu/week3yr4/ErrorHandler.java
@@ -1,4 +1,20 @@
 package ie.atu.week3yr4;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class ErrorHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleValidationExceptions(MethodArgumentNotValidException ex){
+        return "error received" + ex.getBindingResult().getFieldError().getDefaultMessage();
+
+    }
+
+
 }
